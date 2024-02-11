8f89951cb77c3a3975d0fe06b582632a6d9cb85e
Семён Койнов <semen.kojnov.08@gmail.com>
2024-02-11 12:16:53 +0500

    авторизация

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace demo_training_StockProKSV
 {
-    partial class Form1
+    partial class AuthForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -29,12 +29,239 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataSet1 = new demo_training_StockProKSV.DataSet1();
+            this.пользователиBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.пользователиTableAdapter = new demo_training_StockProKSV.DataSet1TableAdapters.ПользователиTableAdapter();
+            this.tableAdapterManager = new demo_training_StockProKSV.DataSet1TableAdapters.TableAdapterManager();
+            this.stockProDB_demoDataSet = new demo_training_StockProKSV.StockProDB_demoDataSet();
+            this.пользователиBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.пользователиTableAdapter1 = new demo_training_StockProKSV.StockProDB_demoDataSetTableAdapters.ПользователиTableAdapter();
+            this.tableAdapterManager1 = new demo_training_StockProKSV.StockProDB_demoDataSetTableAdapters.TableAdapterManager();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.пользователиBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stockProDB_demoDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.пользователиBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(478, 298);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.AutoScroll = true;
+            this.tabPage1.BackColor = System.Drawing.Color.Gainsboro;
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(470, 272);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "АВТОРИЗАЦИЯ";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(470, 272);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "РЕГИСТРАЦИЯ";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(28, 54);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(204, 20);
+            this.textBoxLogin.TabIndex = 0;
+            this.textBoxLogin.Text = "user1";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(28, 113);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(204, 20);
+            this.textBoxPassword.TabIndex = 1;
+            this.textBoxPassword.Text = "password1";
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.BackColor = System.Drawing.Color.White;
+            this.buttonLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonLogin.Location = new System.Drawing.Point(28, 215);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(204, 31);
+            this.buttonLogin.TabIndex = 2;
+            this.buttonLogin.Text = "ВОЙТИ";
+            this.buttonLogin.UseVisualStyleBackColor = false;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(28, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ЛОГИН:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(28, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ПАРОЛЬ";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.buttonLogin);
+            this.panel1.Controls.Add(this.textBoxPassword);
+            this.panel1.Controls.Add(this.textBoxLogin);
+            this.panel1.Location = new System.Drawing.Point(100, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(261, 276);
+            this.panel1.TabIndex = 5;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // пользователиBindingSource
+            // 
+            this.пользователиBindingSource.DataMember = "Пользователи";
+            this.пользователиBindingSource.DataSource = this.dataSet1;
+            // 
+            // пользователиTableAdapter
+            // 
+            this.пользователиTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.sysdiagramsTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = demo_training_StockProKSV.DataSet1TableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            this.tableAdapterManager.Минимальные_Уровни_ТоваровTableAdapter = null;
+            this.tableAdapterManager.ПользователиTableAdapter = this.пользователиTableAdapter;
+            this.tableAdapterManager.ПоставкиTableAdapter = null;
+            this.tableAdapterManager.РасходыTableAdapter = null;
+            this.tableAdapterManager.ТоварыTableAdapter = null;
+            // 
+            // stockProDB_demoDataSet
+            // 
+            this.stockProDB_demoDataSet.DataSetName = "StockProDB_demoDataSet";
+            this.stockProDB_demoDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // пользователиBindingSource1
+            // 
+            this.пользователиBindingSource1.DataMember = "Пользователи";
+            this.пользователиBindingSource1.DataSource = this.stockProDB_demoDataSet;
+            // 
+            // пользователиTableAdapter1
+            // 
+            this.пользователиTableAdapter1.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager1
+            // 
+            this.tableAdapterManager1.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager1.UpdateOrder = demo_training_StockProKSV.StockProDB_demoDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            this.tableAdapterManager1.Минимальные_Уровни_ТоваровTableAdapter = null;
+            this.tableAdapterManager1.ПользователиTableAdapter = this.пользователиTableAdapter1;
+            this.tableAdapterManager1.ПоставкиTableAdapter = null;
+            this.tableAdapterManager1.РасходыTableAdapter = null;
+            this.tableAdapterManager1.ТоварыTableAdapter = null;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MistyRose;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(446, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(21, 22);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "x";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AuthForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(478, 298);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "AuthForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Аутентификация пользоватея. StockPro ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.пользователиBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stockProDB_demoDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.пользователиBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private DataSet1 dataSet1;
+        private System.Windows.Forms.BindingSource пользователиBindingSource;
+        private DataSet1TableAdapters.ПользователиTableAdapter пользователиTableAdapter;
+        private DataSet1TableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TabPage tabPage2;
+        private StockProDB_demoDataSet stockProDB_demoDataSet;
+        private System.Windows.Forms.BindingSource пользователиBindingSource1;
+        private StockProDB_demoDataSetTableAdapters.ПользователиTableAdapter пользователиTableAdapter1;
+        private StockProDB_demoDataSetTableAdapters.TableAdapterManager tableAdapterManager1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,62 @@ using System.Windows.Forms;
 
 namespace demo_training_StockProKSV
 {
-    public partial class Form1 : Form
+    public partial class AuthForm : Form
     {
-        public Form1()
+        public AuthForm()
         {
             InitializeComponent();
+        }
+
+        private void пользователиBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.пользователиBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dataSet1);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "stockProDB_demoDataSet.Пользователи". При необходимости она может быть перемещена или удалена.
+            this.пользователиTableAdapter1.Fill(this.stockProDB_demoDataSet.Пользователи);
+
+
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            string ConnectionStg = "Data Source=WIN-2J5GGL22MAA\\SQLEXPRESS;Initial Catalog=StockProDB-demo;Integrated Security=True;";
+            string login = textBoxLogin.Text;
+            string password = textBoxPassword.Text;
+            using (SqlConnection connection = new SqlConnection(ConnectionStg) )
+            {
+                connection.Open();
+                string authquery = "SELECT COUNT(*) FROM Пользователи WHERE Имя_Пользователя = @Login AND Пароль = @Password";
+                SqlCommand sqlCommand = new SqlCommand(authquery, connection);
+                sqlCommand.Parameters.AddWithValue("Login", login);
+                sqlCommand.Parameters.AddWithValue("Password", password);
+                int count = (int)sqlCommand.ExecuteScalar();
+                {
+                    if (count > 0)
+                    {
+                        MainMenuForm mainMenuForm = new MainMenuForm();
+                        this.Hide();
+                        mainMenuForm.ShowDialog();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Неправильный ввод данных при авторизации!", "Ошибка авторизации", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            this.Close();
         }
     }
 }
